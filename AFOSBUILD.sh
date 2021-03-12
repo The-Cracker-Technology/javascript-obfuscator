c67b722c41a16635f8912488ca6a8865d964076d
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-12 15:19:53 -0300

    Update AFOSBUILD.sh again

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -5,7 +5,7 @@ cp -Rf andraxbin/* /opt/ANDRAX/bin
 chmod -R 755 /opt/ANDRAX/bin
 
 cp -Rf $(pwd) /opt/ANDRAX/javascript-obfuscator
-
+chmod -R 755 /opt/ANDRAX/javascript-obfuscator
 WORKDIR=$(pwd)
 
 cd /opt/ANDRAX/javascript-obfuscator
